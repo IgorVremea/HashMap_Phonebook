d872cb285dea391107b0e02fe731c7a0662b2690
Igor Vremea <igori.vremya@gmail.com>
2024-05-18 23:11:07 +0300

    Creating class Phone Book

--- Task.java
+++ Task.java
@@ -2,14 +2,18 @@ package PhoneBook;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+class PhoneBook{
+    private static HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
 
-public class Task {
-    public static void main(String[] args) {
-        addContact();
+
+    public static void addContact(){
+        String name = readStr("Write name: ");
+        String phoneNum = readStr("Write phone number: ");
+
     }
-
     public static String readStr(String msg){
         try{
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -21,10 +25,11 @@ public class Task {
         }
         return null;
     }
+}
 
-    public static void addContact(){
-        String name = readStr("Write name: ");
-        String phoneNum = readStr("Write phone number: ");
-        System.out.println(name + ": " + phoneNum);
+public class Task {
+    public static void main(String[] args) {
+        PhoneBook book = new PhoneBook();
+        book.addContact();
     }
 }
