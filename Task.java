44e6c5333bebdbd69f1e7c4c83a88286fd8c0e92
Igor Vremea <igori.vremya@gmail.com>
2024-05-18 23:05:36 +0300

    Read data from console

--- Task.java
+++ Task.java
@@ -1,7 +1,30 @@
 package PhoneBook;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 
 public class Task {
     public static void main(String[] args) {
-        
+        addContact();
+    }
+
+    public static String readStr(String msg){
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print(msg);
+            return reader.readLine();
+
+        } catch(IOException ex){
+
+        }
+        return null;
+    }
+
+    public static void addContact(){
+        String name = readStr("Write name: ");
+        String phoneNum = readStr("Write phone number: ");
+        System.out.println(name + ": " + phoneNum);
     }
 }
